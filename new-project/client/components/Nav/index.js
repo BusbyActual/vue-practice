febb52076405ad73e8defc12ea6e838a1f46fd86
Ryan Busby <BusbyActual@gmail.com>
2017-09-12 20:22:04 -0700

    Added folder for Nav component

---
+++ new-project/client/components/Nav/index.js
@@ -0,0 +1,13 @@
+import style from './style.css'
+
+export default {
+  render(h) {
+    return (
+      <div>
+        <button>Home</button>
+        <button>Projects</button>
+        <button>About</button>
+      </div>
+    )
+  }
+}
