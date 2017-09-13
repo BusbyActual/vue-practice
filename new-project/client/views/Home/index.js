d4b28f0f0769e3f2d7ce5735a53ef312d649026c
Ryan Busby <BusbyActual@gmail.com>
2017-09-12 20:19:32 -0700

    Adding nav plug for UI

--- new-project/client/views/Home/index.js
+++ new-project/client/views/Home/index.js
@@ -1,9 +1,11 @@
 import Counter from 'components/Counter'
+import Nav from 'components/Nav'
 
 export default {
   render(h) {
     return (
       <div class="page">
+        <Nav />
         <Counter />
         <p>
           To get started, edit files in <code>./client</code> and save.
