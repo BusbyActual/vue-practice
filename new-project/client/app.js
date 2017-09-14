257321f698b4b163caab36cd81ee0c5f13c26351
Ryan Busby <BusbyActual@gmail.com>
2017-09-13 22:43:15 -0700

    Added vue-material

--- new-project/client/app.js
+++ new-project/client/app.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
 import { sync } from 'vuex-router-sync'
 import App from './components/App'
 import router from './router'
 import store from './store'
 
 sync(store, router)
+Vue.use(VueMaterial)
 
 const app = new Vue({
   router,
